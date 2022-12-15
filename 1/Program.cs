007de22eea3d685911b1aca9f330b91e60096222
Lera <miss_val@bk.ru>
2022-12-15 15:42:08 +0300

    Задача 41

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int cont = 1;
+int biggerthanzero = 0;
+while (cont == 1)
+{
+    Console.WriteLine("Введите числа");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0)
+    {
+        biggerthanzero= biggerthanzero + 1;
+    }
+    Console.WriteLine("Ещё одно? Да = 1, Нет = 0");
+    cont = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Чисел больше 0: {biggerthanzero}");
